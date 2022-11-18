c0a8dc560b0a8f7b485e457862dcc8063a76cc12
d1av <davi4alves@gmail.com>
2022-11-18 18:27:07 -0300

    feat: add new title

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizzComponent implements OnInit {
 
+  public title: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
 
 }
