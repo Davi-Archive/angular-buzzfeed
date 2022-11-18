9d9c6e26f369581c97a7d1412e70d0990d5fd98c
d1av <davi4alves@gmail.com>
2022-11-18 19:26:37 -0300

    feat: add feat reduce method

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -53,9 +53,24 @@ export class QuizzComponent implements OnInit {
     if (this.questionMaxIndex > this.questionIndex) {
       this.questionSelected = this.questions[this.questionIndex]
     } else {
+      const finalAnswer: string = await this.checkResult(this.answers)
       this.finished = true
-      //verificar opção ganhador
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results];
     }
+  }
+
+  async checkResult(anwsers: string[]) {
+    const result = anwsers.reduce((previous, current, i, arr) => {
+      if (
+        arr.filter(item => item === previous).length >
+        arr.filter(item => item === current).length
+      ) {
+        return previous;
+      } else {
+        return current;
+      }
+    })
+    return result;
   }
 
 }
